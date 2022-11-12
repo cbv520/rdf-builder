855934c98b654abd3a3a21cdf37720db6067713f
cbv520 <cblakevillegas@gmail.com>
2022-11-13 08:04:50 +1030

    Update rdf-builder.py

--- rdf-builder.py
+++ rdf-builder.py
@@ -2,38 +2,45 @@ from __future__ import annotations
 import uuid
 
 class RdfGraphBuilder:
-    def __init__(self):
+    def __init__(self, seed=0):
+        self.obj_count = 0
+        self.seed = str(seed)
         pass
     
     def new_object(self, rdf_class: str) -> RdfObjectBuilder:
-        return RdfObjectBuilder(self, rdf_class)
+        obj_hash = uuid.uuid5(uuid.NAMESPACE_URL, self.seed + rdf_class + str(self.obj_count))
+        iri = rdf_class + '_' + str(obj_hash)[-6:]
+        self.obj_count = self.obj_count + 1
+        print(f'{iri} a {rdf_class}')
+        return RdfObjectBuilder(self, iri)
+        
+    def add_to_object(self, iri: str) -> RdfObjectBuilder:
+        return RdfObjectBuilder(self, iri)
 
 class RdfObjectBuilder:
-    def __init__(self, graph_builder: RdfGraphBuilder, rdf_class: str):
-        self.id = rdf_class + '_' + str(uuid.uuid4())[-6:]
+    def __init__(self, graph_builder: RdfGraphBuilder, iri: str):
+        self.iri = iri
         self.graph_builder = graph_builder
-        self.rdf_class = rdf_class
-        print(f'{self.id} a {rdf_class}')
     
     def with_object_property(self, rdf_property: str, rdf_class: str) -> RdfObjectBuilder:
-        obj = self.graph_builder.new_object(rdf_class)
-        obj.parent = self
-        print(f'{self.id} {rdf_property} {obj.id}') 
-        return obj
+        obj_builder = self.graph_builder.new_object(rdf_class)
+        obj_builder.parent_builder = self
+        print(f'{self.iri} {rdf_property} {obj_builder.iri}') 
+        return obj_builder
     
     def with_literal_property(self, rdf_property: str, value: any) -> RdfObjectBuilder:
-        print(f'{self.id} {rdf_property} {value}')
+        print(f'{self.iri} {rdf_property} {value}')
         return self
         
     def with_iri_property(self, rdf_property: str, iri: str) -> RdfObjectBuilder:
-        print(f'{self.id} {rdf_property} {iri}')
+        print(f'{self.iri} {rdf_property} {iri}')
         return self
         
     def end_object_property(self) -> RdfObjectBuilder:
-        return self.parent
+        return self.parent_builder
         
     def end_object(self) -> str:
-        return self.id
+        return self.iri
         
 graph_builder = RdfGraphBuilder()
 cat_iri = graph_builder.new_object('Cat') \
@@ -51,8 +58,10 @@ cat_iri = graph_builder.new_object('Cat') \
                     .end_object_property() \
                 .end_object()
                 
-graph_builder.new_object('Dog') \
+dog_iri = graph_builder.new_object('Dog') \
                 .with_literal_property('hasName', 'Pupper') \
                 .with_iri_property('friendsWith', cat_iri) \
                 .end_object()
-
+                
+graph_builder.add_to_object(cat_iri) \
+                .with_iri_property('hates', dog_iri)
